e18c1548f1488d6b6c4653fe76c1697549aeca3a
Florence Galliers <florence_galliers@hotmail.com>
2021-02-25 10:45:51 +0000

    Update

--- 01-carbon-emissions/app-1/app.R
+++ 01-carbon-emissions/app-1/app.R
@@ -5,6 +5,11 @@
 
 library(shiny)
 library(shinythemes)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(plyr)
+library(waffle)
 
 food_consumption <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-02-18/food_consumption.csv')
 food_consumption$food_category <- factor(food_consumption$food_category)
@@ -31,32 +36,76 @@ food_colours <- c("#e31a1c",
                   "#33a02c",
                   "#b2df8a")
 
+category_names <- c("Beef",
+                    "Eggs",
+                    "Fish",
+                    "Lamb/Goat",
+                    "Dairy",
+                    "Nuts",
+                    "Pork",
+                    "Poultry",
+                    "Rice",
+                    "Soybean",
+                    "Wheat")
+
+footprint_total <- footprint %>%
+  select(country, co2_emmission) %>%
+  group_by(country) %>%
+  dplyr::summarise(total = sum(co2_emmission))
+
+
 # Define a User Interface
 ui <- fluidPage(
   # set theme
-  theme=shinytheme("superhero"),
+  theme=shinytheme("journal"),
+  #shinythemes::themeSelector(),
   # Title
   titlePanel("Carbon Footprint of the World"),
   # Sidebar layout with input and output definitions
-  sidebarLayout(
-    # sidebar panel for inputs
-    sidebarPanel("Select two countries to compare:",
+  fluidRow(
+    
+    # panel for inputs
+    column(12, 
+           "Select two countries from the drop-down menus below and compare the variation in carbon footprint caused by the different food categories. 
+           The information shown is a proportion of the total carbon footprint, which varies from country to country.")),
+  
+  fluidRow(
+    column(6,
       selectInput(inputId = "country1",
                   label = "Choose country 1:",
                   # use countrys, sort into alphabetical order
-                  choices = sort(food_consumption$country)),
+                  choices = sort(food_consumption$country),
+                  selected = sort(food_consumption$country)[1])),
+    column(6, 
     selectInput(inputId = "country2",
                 label = "Choose country 2:",
                 # use countrys, sort into alphabetical order
-                choices = sort(food_consumption$country))),
+                choices = sort(food_consumption$country),
+                selected = sort(food_consumption$country)[1]))
+    ),
     # main panel for displaying outputs
-    mainPanel("Breakdown of carbon footprint into food categories",
-              textOutput(outputId = "text1"),
-              plotOutput(outputId = "plot1"),
-              textOutput(outputId = "text2"),
-              plotOutput(outputId = "plot2")
+    fluidRow(
+      column(6, 
+             # country name
+              h2(textOutput(outputId = "text1")),
+             # total carbon footprint
+             h4(textOutput(outputId = "total1")),
+             # footprint plot
+              plotOutput(outputId = "plot1")),
+      column(6,
+             # country name 2
+              h2(textOutput(outputId = "text2")),
+             # total carbon footprint 2
+             h4(textOutput(outputId = "total2")),
+             # footprint plot 2
+              plotOutput(outputId = "plot2"))),
+    
+    fluidRow(
+      column(6,
+             textOutput(outputId = "text3")),
+      column(6, 
+             textOutput(outputId = "text4"))
     )
-  )
 )
 
 # Define the Server
@@ -64,8 +113,15 @@ server <- function(input, output){
   # use renderplot to indicate that it is reactive and the output type is a plot
  # Output for country 1
    output$text1 <- renderText({
-    paste("Country 1:", input$country1)
+    paste(input$country1)
   })
+   # total for country 1
+   output$total1 <- renderText({
+     paste("Total carbon footprint:",
+       footprint_total[footprint_total$country == input$country1, 2], 
+       "kg/person/year"
+     )
+   })
    # output plot for country 1
    output$plot1 <- renderPlot({
      ggplot(footprint, 
@@ -76,47 +132,36 @@ server <- function(input, output){
                           values = co2_emmission,
                           color = food_category),
                       flip = TRUE,
-                      size = 10,
+                      size = 8,
                       make_proportional = T,
                       data = footprint[footprint$country == input$country1, 2:3]) +
        scale_label_pictogram(
          name = NULL,
          values = "shoe-prints",
-         labels = c("Beef",
-                    "Eggs",
-                    "Fish",
-                    "Lamb/Goat",
-                    "Dairy",
-                    "Nuts",
-                    "Pork",
-                    "Poultry",
-                    "Rice",
-                    "Soybean",
-                    "Wheat")
+         labels = category_names
        ) +
        scale_color_manual(
          name = NULL,
          values = food_colours,
-         labels = c("Beef",
-                    "Eggs",
-                    "Fish",
-                    "Lamb/Goat",
-                    "Dairy",
-                    "Nuts",
-                    "Pork",
-                    "Poultry",
-                    "Rice",
-                    "Soybean",
-                    "Wheat")
+         labels = category_names
        ) +
        coord_equal() +
        theme_minimal() +
-       theme(panel.grid = element_blank())
+       theme(panel.grid = element_blank(),
+             axis.text = element_blank(),
+             legend.position = "none")
      
   })
    # output text for country 2
    output$text2 <- renderText({
-     paste("Country 2:", input$country2)
+     paste(input$country2)
+   })
+   # total for country 2
+   output$total2 <- renderText({
+     paste("Total carbon footprint:",
+           footprint_total[footprint_total$country == input$country2, 2], 
+           "kg/person/year"
+     )
    })
    # output plot for country 2
   output$plot2 <- renderPlot({
@@ -127,45 +172,32 @@ server <- function(input, output){
                      aes(label = food_category,
                          values = co2_emmission,
                          color = food_category),
-                     flip = TRUE,
-                     size = 10,
+                     flip = T,
+                     size = 8,
                      make_proportional = T,
                      data = footprint[footprint$country == input$country2, 2:3]) +
       scale_label_pictogram(
         name = NULL,
         values = "shoe-prints",
-        labels = c("Beef",
-                   "Eggs",
-                   "Fish",
-                   "Lamb/Goat",
-                   "Dairy",
-                   "Nuts",
-                   "Pork",
-                   "Poultry",
-                   "Rice",
-                   "Soybean",
-                   "Wheat")
+        labels = category_names
       ) +
       scale_color_manual(
         name = NULL,
         values = food_colours,
-        labels = c("Beef",
-                   "Eggs",
-                   "Fish",
-                   "Lamb/Goat",
-                   "Dairy",
-                   "Nuts",
-                   "Pork",
-                   "Poultry",
-                   "Rice",
-                   "Soybean",
-                   "Wheat")
+        labels = category_names
       ) +
       coord_equal() +
       theme_minimal() +
-      theme(panel.grid = element_blank())
-    
+      theme(panel.grid = element_blank(),
+            axis.text = element_blank(),
+            legend.position = "none")
   })
+    
+    # output text 3
+    output$text3 <- renderText({
+      paste("Country 1:", input$country1)
+    })
+  
 }
 
 # Run the App
